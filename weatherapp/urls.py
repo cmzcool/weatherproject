5c3b65ec716a2399142ee5f41f7179e76e94422c
cmzcool <85162558+cmzcool@users.noreply.github.com>
2021-08-19 23:35:55 +1000

    Add files via upload

---
+++ weatherapp/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+
+from . import views
+
+app_name = 'weatherapp'
+urlpatterns = [
+    path('', views.weather, name='weather'),
+    path('', views.home, name='home'),
+
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+
